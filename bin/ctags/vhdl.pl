ba963ec4bd96f34421a5a0e7928e9fef83442acc
THONNART Yvain 210879 <yvain.thonnart@cea.fr>
2017-07-24 09:04:01 +0200

    update patterns to include signatures for codegentemplates

--- bin/ctags/vhdl.pl
+++ bin/ctags/vhdl.pl
@@ -39,44 +39,44 @@ while(<>) {
     $address=$_;
     $line=$.;
 
-    if (/^\s*entity\s+(\w+)\s+is/i) { $name=$1; $kind='e';
-        print "$name\t$file\t/^$address/;\"\tkind:$kind\tfile:\tline:$line\n";
+    if (/^\s*entity\s+((?:<[^>]*>)?(\w+)(?:\S*))\s+is/i) { $name=$2; $kind='e'; $sig=($1 eq $2)?"":"\tsignature:: $1";
+        print "$name\t$file\t/^$address/;\"\tkind:$kind\tfile:\tline:$line$sig\n";
         $curscope=$name; pushscope(\$scope,$curscope);
         $kscope=$kind2scope{$kind}; 
 
     } elsif (/^\s*generic\s*\(/i) { $kind='g';
-    } elsif ($kscope eq 'entity' and $kind eq 'g' and /^\s*(\w+)\s*:/i) { $name=$1;
-        print "$name\t$file\t/^$address/;\"\tkind:$kind\tfile:\tline:$line\t$kscope:$scope\::generics\n";
+    } elsif ($kscope eq 'entity' and $kind eq 'g' and /^\s*((?:<[^>]*>)?(\w+)(?:\S*))\s*:/i) { $name=$2; $sig=($1 eq $2)?"":"\tsignature:: $1";
+        print "$name\t$file\t/^$address/;\"\tkind:$kind\tfile:\tline:$line\t$kscope:$scope\::generics$sig\n";
 
     } elsif (/^\s*port\s*\(/i) { $kind='p';
-    } elsif ($kscope eq 'entity' and $kind eq 'p' and /^\s*(\w+)\s*:\s*(in|out)/i) { $name=$1;$sig=" (".lc($2).")";
-        print "$name\t$file\t/^$address/;\"\tkind:$kind\tfile:\tline:$line\t$kscope:$scope\::ports\tsignature:$sig\n";
+    } elsif ($kscope eq 'entity' and $kind eq 'p' and /^\s*((?:<[^>]*>)?(\w+)(?:\S*))\s*:\s*(in|out)/i) { $name=$2;$sig="\tsignature: (".lc($3).")".(($1 eq $2)?"":": $1");
+        print "$name\t$file\t/^$address/;\"\tkind:$kind\tfile:\tline:$line\t$kscope:$scope\::ports$sig\n";
 
     } elsif (/^\s*end\s+(entity\s+)?$curscope/i) { popscope(\$scope);
 
-    } elsif (/^\s*architecture\s+(\w+)\s+of\s+(\w+)\s+is/i) { $name=$1; $kind='a';
-        $curscope=$2; $kscope=$kind2scope{'e'}; pushscope(\$scope,$curscope);
-        print "$name\t$file\t/^$address/;\"\tkind:$kind\tfile:\tline:$line\t$kscope:$scope\n";
+    } elsif (/^\s*architecture\s+((?:<[^>]*>)?(\w+)(?:\S*))\s+of\s+((?:<[^>]*>)?(\w+)(?:\S*))\s+is/i) { $name=$2; $kind='a';$sig=($1 eq $2)?"":"\tsignature:: $1";
+        $curscope=$4; $kscope=$kind2scope{'e'}; pushscope(\$scope,$curscope);
+        print "$name\t$file\t/^$address/;\"\tkind:$kind\tfile:\tline:$line\t$kscope:$scope$sig\n";
         $curscope=$name; pushscope(\$scope,$curscope);
         $kscope=$kind2scope{$kind}; 
         $scope_body=0;
 
-    } elsif (/^\s*type\s*(\w+)\s*is/i) { $name=$1; $kind='t';
-        print "$name\t$file\t/^$address/;\"\tkind:$kind\tfile:\tline:$line\t$kscope:$scope\::types\n";
+    } elsif (/^\s*type\s*((?:<[^>]*>)?(\w+)(?:\S*))\s*is/i) { $name=$2; $kind='t'; $sig=($1 eq $2)?"":"\tsignature:: $1";
+        print "$name\t$file\t/^$address/;\"\tkind:$kind\tfile:\tline:$line\t$kscope:$scope\::types$sig\n";
 
-    } elsif (/^\s*signal\s*(\w+)\s*:/i) { $name=$1; $kind='s';
-        print "$name\t$file\t/^$address/;\"\tkind:$kind\tfile:\tline:$line\t$kscope:$scope\::signals\n";
+    } elsif (/^\s*signal\s*((?:<[^>]*>)?(\w+)(?:\S*))\s*:/i) { $name=$2; $kind='s'; $sig=($1 eq $2)?"":"\tsignature:: $1";
+        print "$name\t$file\t/^$address/;\"\tkind:$kind\tfile:\tline:$line\t$kscope:$scope\::signals$sig\n";
 
-    } elsif (/^\s*component\s*(\w+)/i) { $name=$1; $kind='c';
-        print "$name\t$file\t/^$address/;\"\tkind:$kind\tfile:\tline:$line\t$kscope:$scope\::components\n";
+    } elsif (/^\s*component\s*((?:<[^>]*>)?(\w+)(?:\S*))/i) { $name=$2; $kind='c'; $sig=($1 eq $2)?"":"\tsignature:: $1";
+        print "$name\t$file\t/^$address/;\"\tkind:$kind\tfile:\tline:$line\t$kscope:$scope\::components$sig\n";
 
     } elsif ($kscope eq 'architecture' and /^\s*begin\b/i) { $scope_body=1;
 
-    } elsif ($kscope eq 'architecture' and $scope_body==1 and /^\s*(?:(\w+)\s*:\s*)?process\s*(\(.*\))?/i) { $name=$1?$1:"line$."; $kind='r';$sig=" $2";
-        print "$name\t$file\t/^$address/;\"\tkind:$kind\tfile:\tline:$line\t$kscope:$scope\::processes\tsignature:$sig\n";
+    } elsif ($kscope eq 'architecture' and $scope_body==1 and /^\s*(?:((?:<[^>]*>)?(\w+)(?:\S*))\s*:\s*)?process\s*(\(.*\))?/i) { $name=$2?$2:"line$."; $kind='r';$sig="\tsignature: $3".(($1 eq $2)?"":": $1");
+        print "$name\t$file\t/^$address/;\"\tkind:$kind\tfile:\tline:$line\t$kscope:$scope\::processes$sig\n";
 
-    } elsif ($kscope eq 'architecture' and $scope_body==1 and /^\s*(\w+)\s*:\s*(\w+)/i) { $name=$1; $kind='i';$sig=" ($2)";
-        print "$name\t$file\t/^$address/;\"\tkind:$kind\tfile:\tline:$line\t$kscope:$scope\::instances\tsignature:$sig\n";
+    } elsif ($kscope eq 'architecture' and $scope_body==1 and /^\s*((?:<[^>]*>)?(\w+)(?:\S*))\s*:\s*((?:<[^>]*>)?(\w+)(?:\S*))/i) { $name=$2; $kind='i';$sig=" ($3)".(($1 eq $2)?"":": $1");
+        print "$name\t$file\t/^$address/;\"\tkind:$kind\tfile:\tline:$line\t$kscope:$scope\::instances$sig\n";
 
     } elsif (/^\s*end\s+(architecture\s+)?$curscope/i) { popscope(\$scope);
 
